3e2d38915ef9a454222e46996cdbee8a0ce89770
maestro <dream.big.dev.423@gmail.com>
2019-07-09 18:37:29 +0300

    add basic functionality

--- src/store.js
+++ src/store.js
@@ -5,12 +5,23 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
-
+    pageInfo: [],
+    curPage: 0,
+    colors: [
+      '#130656', '#584EA1', '#3F8DCC', '#00BED2', '#48B85E', '#82C55D', '#FFCB05', '#FEBE2A', '#F8962A', '#F04E45', '#413B3D', '#413B3D', '#413B3D'
+    ]
   },
   mutations: {
-
+    SET_DATA (state, data) {
+      state.pageInfo = data
+    }
   },
   actions: {
-
+    setData (context, data) {
+      context.commit('SET_DATA', data)
+    }
+  },
+  getters: {
+    pageInfo: state => state.pageInfo[state.curPage]
   }
 })
