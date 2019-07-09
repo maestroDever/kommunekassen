33e63976ededbbf5618aba043c243ef020e1ce86
maestro <dream.big.dev.423@gmail.com>
2019-07-09 18:37:10 +0300

    create action and progressbar component

---
+++ src/components/Action.vue
@@ -0,0 +1,48 @@
+<template>
+  <div class="action">
+    <div v-if="content.type === 'integer'">
+      <button
+        class="button"
+        v-for="one in content.oneOf"
+        :key="`${one.const}-${one.active}`"
+        @click="handleClick(one.const)"
+        :class="one.active ? 'active' : ''"
+      >
+        {{ one.title }}
+      </button>
+    </div>
+    <div v-else style="width: 100%">
+      <input type="number" class="answer-input">
+      <span style="display: block; margin: 10px;">mio. kr.</span>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Action',
+  props: ['content'],
+  methods: {
+    handleClick (id) {
+      console.log(id)
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+  .action {
+    display: flex;
+    flex-wrap: wrap;
+    margin-top: 20px;
+
+    .answer-input {
+      border: none;
+      border-bottom: #eee solid 3px;
+      line-height: 2;
+      font-size: 36px;
+      text-align: center;
+      caret-color: #231F20;
+    }
+  }
+</style>
---
+++ src/components/ProgressBar.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="footer--progress-bar">
+    <div class="cell" v-for="n in curPage" :key="n" :style="`background-color: ${colors[n]}`"></div>
+  </div>
+</template>
+
+<script>
+import { mapGetters, mapState } from 'vuex';
+export default {
+  name: 'ProgressBar',
+  computed: {
+    ...mapState(['curPage', 'colors']),
+  }
+}
+</script>
