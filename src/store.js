ded2b08063f0a689aca38970788490bbf545b2e1
maestro <dream.big.dev.423@gmail.com>
2019-07-10 19:44:52 +0300

    add icon classes in state

--- src/store.js
+++ src/store.js
@@ -6,9 +6,30 @@ Vue.use(Vuex)
 export default new Vuex.Store({
   state: {
     pageInfo: [],
+    blankPage: {
+      title: 'waiting',
+      description: 'loading...'
+    },
     curPage: 0,
     colors: [
       '#130656', '#584EA1', '#3F8DCC', '#00BED2', '#48B85E', '#82C55D', '#FFCB05', '#FEBE2A', '#F8962A', '#F04E45', '#413B3D', '#413B3D', '#413B3D'
+    ],
+    icons: [
+      'icon_scenarios',
+      'icon_city_council',
+      'icon_scenarios',
+      'icon_budget_daycare',
+      'icon_budget_culture_and_freetime',
+      'icon_budget_traffic_and_ways',
+      'icon_budget_nature_and_climate',
+      'icon_budget_health',
+      'icon_budget_cash_and_unemployment_benefits',
+      'icon_budget_town_hall_and_administration',
+      'icon_budget_elderly_and_disabled',
+      'icon_budget_business_and_entrepreneurship',
+      'icon_projects_concert_venue',
+      'icon_projects_dining_houses_in_schools',
+      'icon_city_council'
     ]
   },
   mutations: {
@@ -22,6 +43,6 @@ export default new Vuex.Store({
     }
   },
   getters: {
-    pageInfo: state => state.pageInfo[state.curPage]
+    pageInfo: state => state.pageInfo[state.curPage] || state.blankPage
   }
 })
