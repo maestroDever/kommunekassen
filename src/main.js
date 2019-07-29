e5c1ec3a900c155991f01f5035e5ab782aeec0d6
maestro <dream.big.dev.423@gmail.com>
2019-07-29 06:39:54 +0300

    implement vue-custom-element

--- src/main.js
+++ src/main.js
@@ -3,12 +3,17 @@ import App from './App.vue'
 import store from './store'
 
 import VTooltip from 'v-tooltip'
+import VueCustomElement from 'vue-custom-element'
 
 Vue.use(VTooltip)
+Vue.use(VueCustomElement)
 
 Vue.config.productionTip = false
 
-new Vue({
-  store,
-  render: h => h(App)
-}).$mount('#app')
+// new Vue({
+//   store,
+//   render: h => h(App)
+// }).$mount('#app')
+
+App.store = store
+Vue.customElement('vue-kommunekassen', App)
