db5a17a858a295d085910e2196cc44205ac477d6
maestro <dream.big.dev.423@gmail.com>
2019-07-31 07:12:45 +0300

    wrap all styles in vue-kommunekassen

--- src/App.vue
+++ src/App.vue
@@ -27,7 +27,8 @@ export default {
 </script>
 
 <style lang="scss">
-@import url(./assets/icons/css/kommunekassen-icons.css);
+@import './assets/css/main.scss';
+@import './assets/icons/css/kommunekassen-icons.css';
 #app {
   font-family: 'DINNextW01-Bold';
   -webkit-font-smoothing: antialiased;
