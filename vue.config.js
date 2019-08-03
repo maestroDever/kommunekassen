98dcf3368fc770605614a1d3f393f163791ed91a
maestro <dream.big.dev.423@gmail.com>
2019-08-03 04:29:30 +0300

    add build option in vue config

--- vue.config.js
+++ vue.config.js
@@ -1,14 +1,36 @@
 const webpack = require('webpack')
+let assetsDir = 'assets'
+
 module.exports = {
+  assetsDir: assetsDir,
+
   configureWebpack: {
     plugins: [
       new webpack.optimize.LimitChunkCountPlugin({
         maxChunks: 1
       })
-    ]
-  },
-  chainWebpack:
-    config => {
-      config.optimization.delete('splitChunks')
+    ],
+    output: {
+      filename: assetsDir + '/kommunekassen-[name].js',
+      chunkFilename: assetsDir + '/kommunekassen-[name].js'
     }
+  },
+  chainWebpack: config => {
+    if (config.plugins.has('extract-css')) {
+      const extractCSSPlugin = config.plugin('extract-css')
+      extractCSSPlugin &&
+        extractCSSPlugin.tap(() => [
+          {
+            filename: assetsDir + '/kommunekassen-[name].css',
+            chunkFilename: assetsDir + '/kommunekassen-[name].css'
+          }
+        ])
+    }
+    config.optimization.delete('splitChunks')
+
+    // config.plugins
+    //   .delete('html')
+    //   .delete('prefetch')
+    //   .delete('preload')
+  }
 }
